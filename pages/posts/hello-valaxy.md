8349be7bc8d0dc31ee83dfef6f6d8d45fe72304e
黎可Rik <like-rik@outlook.com>
2024-03-12 21:06:08 +0800

    初次提交

---
+++ pages/posts/hello-valaxy.md
@@ -0,0 +1,44 @@
+---
+title: Valaxy 个人小记
+# date: 2024-03-12
+# updated: 2024-03-12
+categories: 
+  - 编程
+tags:
+  - 博客框架
+copyright: false
+# aplayer: true
+# type: bilibili
+excerpt:
+excerpt_type: text
+---
+
+## 准备
+### 环境
+- Node 14.18+
+- pnpm
+
+::: details 选择 pnpm 的原因
+
+npm 会缓存此前下载的版本，因此可能
+
+:::
+
+### 安装
+::: code-group
+```sh [创建项目]
+pnpm create valaxy
+```
+
+```sh [创建项目]
+cd valaxy-blog
+pnpm i
+pnpm dev
+```
+:::
+
+## 使用
+### 写作
+[语言列表](https://shiki.style/languages#bundled-languages)
+
+### 部署
---
+++ valaxy.config.ts
@@ -0,0 +1,95 @@
+import { defineValaxyConfig } from 'valaxy'
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: ['黎可', 'の','世界'],
+      cloud: {
+        enable: true,
+      },
+    },
+
+    pages: [
+      // {
+      //   name: '我的小伙伴们',
+      //   url: '/links/',
+      //   icon: 'i-ri-genderless-line',
+      //   color: 'dodgerblue',
+      // },
+      // {
+      //   name: '喜欢的女孩子',
+      //   url: '/girls/',
+      //   icon: 'i-ri-women-line',
+      //   color: 'hotpink',
+      // },
+    ],
+
+    footer: {
+      since: 2024,
+      powered: false,
+      icon: {
+        // enable: true,
+        name: 'i-ri-heart-line',
+        animated: true,
+        color: '#BB2155',
+        url: 'javascript:void(0)',
+        title: '爱你'
+      },
+      beian: {
+        enable: false,
+        icp: '苏ICP备17038157号',
+      },
+    },
+
+    fireworks: {
+      enable: true,
+    },
+
+    // outlineTitle: '',
+
+    say: {
+      enable: false,
+      api: '',
+      hitokoto: {
+        enable: false,
+        api: ''
+      }
+    },
+
+    notice: {
+      enable: false,
+      hideInPages: false,
+      content: '公告！'
+    },
+
+    menu: {
+      /**
+       * @zh 最右侧的导航图标
+       */
+      custom: {
+        title: '感谢',
+        url: 'string',
+        icon: 'i-ri-heart-line',
+      }
+    }
+
+  },
+
+  unocss: { safelist },
+
+
+})
